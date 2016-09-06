b5b6cc201a3b7e0cd1ce7d13b329af462b480269
Murali Saripalli <murali@smurali.com>
2016-09-07 01:48:34 +1000

    Version 1 - Sample run done and working

---
+++ AnalyseOMR/AnalyseOMR.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EvaluateOMR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.statusMsg = New System.Windows.Forms.Label()
+        Me.btnAnalyse = New System.Windows.Forms.Button()
+        Me.fromOMR = New System.Windows.Forms.TextBox()
+        Me.toOMR = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.rangeSelection = New System.Windows.Forms.GroupBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.rangeSelection.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.AccessibleName = ""
+        Me.btnBrowse.Location = New System.Drawing.Point(8, 105)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 0
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'statusMsg
+        '
+        Me.statusMsg.AutoEllipsis = True
+        Me.statusMsg.Location = New System.Drawing.Point(8, 9)
+        Me.statusMsg.Name = "statusMsg"
+        Me.statusMsg.Size = New System.Drawing.Size(472, 84)
+        Me.statusMsg.TabIndex = 1
+        Me.statusMsg.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'btnAnalyse
+        '
+        Me.btnAnalyse.AccessibleName = ""
+        Me.btnAnalyse.Location = New System.Drawing.Point(8, 105)
+        Me.btnAnalyse.Name = "btnAnalyse"
+        Me.btnAnalyse.Size = New System.Drawing.Size(75, 23)
+        Me.btnAnalyse.TabIndex = 2
+        Me.btnAnalyse.Text = "Evaluate"
+        Me.btnAnalyse.UseVisualStyleBackColor = True
+        '
+        'fromOMR
+        '
+        Me.fromOMR.Location = New System.Drawing.Point(71, 12)
+        Me.fromOMR.Name = "fromOMR"
+        Me.fromOMR.Size = New System.Drawing.Size(56, 20)
+        Me.fromOMR.TabIndex = 3
+        '
+        'toOMR
+        '
+        Me.toOMR.Location = New System.Drawing.Point(199, 12)
+        Me.toOMR.Name = "toOMR"
+        Me.toOMR.Size = New System.Drawing.Size(56, 20)
+        Me.toOMR.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "From OMR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(145, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "To OMR"
+        '
+        'rangeSelection
+        '
+        Me.rangeSelection.Controls.Add(Me.toOMR)
+        Me.rangeSelection.Controls.Add(Me.Label1)
+        Me.rangeSelection.Controls.Add(Me.Label2)
+        Me.rangeSelection.Controls.Add(Me.fromOMR)
+        Me.rangeSelection.Location = New System.Drawing.Point(100, 96)
+        Me.rangeSelection.Name = "rangeSelection"
+        Me.rangeSelection.Size = New System.Drawing.Size(263, 38)
+        Me.rangeSelection.TabIndex = 7
+        Me.rangeSelection.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'EvaluateOMR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(488, 138)
+        Me.Controls.Add(Me.rangeSelection)
+        Me.Controls.Add(Me.statusMsg)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnAnalyse)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "EvaluateOMR"
+        Me.Text = "Evaluate OMR Results"
+        Me.rangeSelection.ResumeLayout(False)
+        Me.rangeSelection.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents statusMsg As Label
+    Friend WithEvents btnAnalyse As Button
+    Friend WithEvents fromOMR As TextBox
+    Friend WithEvents toOMR As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents rangeSelection As GroupBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ AnalyseOMR/AnalyseOMR.vb
@@ -0,0 +1,200 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class EvaluateOMR
+
+    Dim srcExcel As New Excel.Application
+    Dim srcWB As Excel.Workbook
+    Dim srcS As Excel.Worksheet
+
+    Dim targetExcel As Excel.Application
+    Dim targetWB As Excel.Workbook
+    Dim targetS As Excel.Worksheet
+
+    Dim fixedMsg As String
+
+    Dim fileName As String
+    Dim totOMR, totQ As Int16
+
+    Dim startOMR, endOMR As Int16
+    Dim currentRow As Int16
+    Dim r As New List(Of String)
+    Dim w As New List(Of String)
+    Dim u As New List(Of String)
+    Dim b As New List(Of String)
+    Dim targetRow = 2
+
+    Private Sub AnalyseOMR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnAnalyse.Visible = False
+        rangeSelection.Visible = False
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        fixedMsg = ""
+        totOMR = 0
+        totQ = 0
+
+        Dim browseFile As New OpenFileDialog()
+        browseFile.Filter = "Excel files (*.xls, *.xlsx)|*.xls;*.xlsx"
+
+        If browseFile.ShowDialog() =
+            DialogResult.OK Then
+            If Dir(browseFile.FileName) <> "" Then
+                fileName = browseFile.FileName
+
+                ShowMessage("Loading " & fileName)
+                btnBrowse.Visible = False
+
+                srcWB = srcExcel.Workbooks.Open(fileName)
+                srcS = srcWB.Worksheets(1)
+                FixMessage(fileName & " Loaded")
+
+                'Counting total questions ==>
+                ShowMessage("Counting questions..")
+                Dim row = 1, column = 2
+                While Not String.IsNullOrEmpty(srcS.Cells(row, column).Value)
+                    totQ = totQ + 1
+                    column = column + 1
+                End While
+                FixMessage("Total questions: " & totQ)
+                '<== Counting total questions
+
+                'Counting total OMRs ==>
+                ShowMessage("Counting OMRs..")
+                row = 2
+                column = 1
+                While Not String.IsNullOrEmpty(srcS.Cells(row, column).Value)
+                    totOMR = totOMR + 1
+                    row = row + 1
+                End While
+                FixMessage("Total OMRs: " & totOMR)
+                '<== Counting total OMRs
+
+                fromOMR.Text = 1
+                toOMR.Text = totOMR
+                rangeSelection.Visible = True
+                ShowMessage("Change From and To values if required and click Evaluate to continue")
+                btnAnalyse.Visible = True
+            Else
+                ShowMessage("File not found")
+            End If
+        End If
+    End Sub
+
+    Private Sub AnalyseOMR_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        srcExcel.Quit()
+    End Sub
+
+    Private Function IsValidEntry() As Boolean
+        If IsNumeric(fromOMR.Text) And IsNumeric(toOMR.Text) Then
+            startOMR = fromOMR.Text
+            endOMR = toOMR.Text
+
+            If startOMR <= 0 Or endOMR > totOMR Or startOMR > endOMR Then
+                MessageBox.Show("Enter numbers in valid range")
+                Return False
+            End If
+        Else
+            MessageBox.Show("Enter valid numbers")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub btnAnalyse_Click(sender As Object, e As EventArgs) Handles btnAnalyse.Click
+        If Not IsValidEntry() Then
+            Return
+        End If
+
+        rangeSelection.Visible = False
+        btnAnalyse.Visible = False
+
+        'Generating Excel Template
+        ShowMessage("Generating excel template")
+        targetExcel = CreateObject("Excel.Application")
+        targetWB = targetExcel.Workbooks.Add()
+        targetS = targetWB.Sheets.Add()
+        targetExcel.DisplayAlerts = False
+        targetS.Cells(1, 1).Value = "OMR NO"
+        targetS.Cells(1, 2).Value = "R"
+        targetS.Cells(1, 3).Value = "W"
+        targetS.Cells(1, 4).Value = "U"
+
+        currentRow = startOMR + 1
+        targetRow = 2
+        Timer1.Start()
+    End Sub
+
+    Private Sub saveExcel()
+        targetS.Columns("A:E").AutoFit()
+
+        ' Showing save file dialog
+        Dim saveFile As New SaveFileDialog()
+        saveFile.DefaultExt = "xls"
+        saveFile.AddExtension = True
+        If startOMR = 1 And endOMR = totOMR Then
+            saveFile.FileName = "Analysis_" & System.IO.Path.GetFileName(fileName)
+        Else
+            saveFile.FileName = "Analysis(" & startOMR & "-" & endOMR & ")_" & System.IO.Path.GetFileName(fileName)
+        End If
+        saveFile.Title = "Save the analysed file"
+        If saveFile.ShowDialog <> 2 Then
+            fileName = saveFile.FileName
+            ShowMessage("Saving..")
+            targetWB.SaveAs(fileName)
+            targetExcel.Quit()
+            FixMessage("Analysed file saved To " & fileName)
+        Else
+            FixMessage("Invalid save path")
+        End If
+
+        srcExcel.Quit()
+        ShowMessage("Browse to start again")
+        btnBrowse.Visible = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Dim omrNum = srcS.Cells(currentRow, 1).Value
+        ShowMessage("Evaluating OMR " & (currentRow - 1) & "/" & totOMR & "(" & omrNum & ")")
+        targetS.Cells(targetRow, 1).Value = omrNum
+        For column = 2 To totQ + 1
+            Dim qResult = LCase(srcS.Cells(currentRow, column).Value)
+            Dim qNum = srcS.Cells(1, column).Value
+            If qResult = "r" Then
+                r.Add(qNum)
+            ElseIf qResult = "w" Then
+                w.Add(qNum)
+            ElseIf qResult = "u" Then
+                u.Add(qNum)
+            Else
+                b.Add(qNum)
+            End If
+        Next
+        targetS.Cells(targetRow, 2).Value = String.Join(",", r)
+        targetS.Cells(targetRow, 3).Value = String.Join(",", w)
+        targetS.Cells(targetRow, 4).Value = String.Join(",", u)
+        targetS.Cells(targetRow, 5).Value = String.Join(",", b)
+        r.Clear()
+        w.Clear()
+        u.Clear()
+        b.Clear()
+        targetRow = targetRow + 1
+        currentRow = currentRow + 1
+        If currentRow > endOMR + 1 Then
+            saveExcel()
+        Else
+            Timer1.Start()
+        End If
+    End Sub
+
+    Private Sub ShowMessage(msg As String)
+        statusMsg.Text = fixedMsg & vbNewLine & msg
+    End Sub
+
+    Private Sub FixMessage(msg As String)
+        fixedMsg = fixedMsg & msg & vbNewLine
+        statusMsg.Text = fixedMsg
+    End Sub
+
+End Class
+
